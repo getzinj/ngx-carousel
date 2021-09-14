4e6eacbde905a5bdb9d75e400031b904a4b82ddb
Jeffrey Getzin <getzinj@gmail.com>
2021-09-14 01:01:16 -0400

    Fixed public-api

--- projects/ngx-carousel/src/public-api.ts
+++ projects/ngx-carousel/src/public-api.ts
@@ -2,6 +2,6 @@
  * Public API Surface of ngx-carousel
  */
 
-export * from './lib/ngx-carousel.service';
-export * from './lib/ngx-carousel.component';
-export * from './lib/ngx-carousel.module';
+export * from './lib/ngx-carousel/ngx-carousel.component';
+export * from './lib/ngx-item/ngx-item.component';
+export * from './lib/ngx-tile/ngx-tile.component';
